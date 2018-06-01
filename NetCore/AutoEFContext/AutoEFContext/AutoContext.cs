1b33749a3f05b69f7f2f2b111b1bb95e6f783add
Woody-Hu <812791840@qq.com>
2018-06-01 13:07:02 +0800

    2018 06 01

---
+++ NetCore/AutoEFContext/AutoEFContext/AutoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace AutoEFContext
+{
+    public abstract class AutoContext:DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
